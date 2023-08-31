1e89a94e737bfdf5f4f50a030a7e266738c1378c
Renato Cordeiro Ferreira <renato.cferreira@hotmail.com>
2023-08-31 15:13:43 +0200

    Install module `@nuxtjs/i18n`
    Add integration with [vue-i18n][1] plugin via [nuxt-i18n][2] module.
    
    Configure `pt_BR` and `en_US` as default languages. Use `pt_BR` as the
    default language.
    
    Choose `prefix` strategy so all pages, so that content in all localized
    pages has a prefix, but pages without prefix redirect to the default
    language.
    
    [1]: https://vue-i18n.intlify.dev
    [2]: https://v8.i18n.nuxtjs.org

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/google-fonts", "@nuxtjs/tailwindcss"],
+  modules: ["@nuxtjs/google-fonts", "@nuxtjs/tailwindcss", "@nuxtjs/i18n"],
   googleFonts: {
     families: { "Cinzel+Decorative": true, Quicksand: true },
+  },
+  i18n: {
+    locales: ["pt_BR", "en_US"],
+    defaultLocale: "pt_BR",
+    strategy: "prefix",
+    vueI18n: "./i18n.config.ts",
   },
 });
