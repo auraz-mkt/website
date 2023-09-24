46030577ad7d7f849af003140b255a117edaf38f
Gustavo Ueti Fukunaga <gustavofukunaga@usp.br>
2023-09-24 04:07:04 -0300

    Add social share component and alert component

---
+++ components/Alert.vue
@@ -0,0 +1,29 @@
+<template>
+    <div v-if="show"
+        class=" fixed top-4 left-4 p-4 text-light rounded-lg bg-dark">
+        {{ message }}
+    </div>
+</template>
+  
+<script>
+export default {
+    data() {
+        return {
+            show: false,
+            message: '',
+        };
+    },
+    methods: {
+        showAlert(message) {
+            this.message = message;
+            this.show = true;
+            setTimeout(() => {
+                this.show = false;
+            }, 4000);
+        },
+    },
+};
+</script>
+  
+<style></style>
+  
