d34fe30b9ff6a54aa595280e1740082b35050a5a
Renato Cordeiro Ferreira <renato.cferreira@hotmail.com>
2023-09-27 11:29:07 +0200

    Install module `nuxt-icon`

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/google-fonts", "@nuxtjs/tailwindcss", "@nuxtjs/i18n"],
+  modules: [
+    "@nuxtjs/google-fonts",
+    "@nuxtjs/tailwindcss",
+    "@nuxtjs/i18n",
+    "nuxt-icon",
+  ],
   googleFonts: {
     families: { "Cinzel+Decorative": true, Quicksand: true },
   },
