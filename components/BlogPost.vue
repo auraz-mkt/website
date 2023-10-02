2ee5e00e31701d5c062b80638bab5b7b6accc394
Gustavo Ueti Fukunaga <gustavofukunaga@usp.br>
2023-10-02 18:15:14 -0300

    Add post details page integration with contentful

--- components/BlogPost.vue
+++ components/BlogPost.vue
@@ -1,32 +1,55 @@
-<template>
-  <div class="blog_post">
-    <h1 class="blog_post__title">{{ title }}</h1>
+<template class="mx-7 lg:mx-6 mt-32 flex-grow">
+  <article class="max-w-5xl mx-auto">
+    <header class="mb-14">
+      <div class="mt-3 text-center">
 
-    <p class="blog_post__dates">
-      {{ $t("blog.publication") }}:
-      <span class="blog_post__publication">{{ publicationDate }}</span>
-      - {{ $t("blog.revision") }}:
-      <span class="blog_post__revision">{{ revisionDate }}</span>
-    </p>
+        <a href="#" class="inline-block bg-slate-200 rounded-full px-3 py-1 text-sm font-medium text-slate-700 m-0.5">
+          {{ $t("blog.tags") }}:
+          {{ tagsList }}</a>
+
+      </div>
+
+      <h1 class="text-4xl text-center font-bold leading-normal text-slate-900 mt-0 mb-3">
+        {{ title }}</h1>
+
+      <div class="relative mt-4 flex items-center gap-x-4 justify-center text-center">
+        <img :src="author[0].fields.image['pt-BR'].fields.file['pt-BR'].url" alt=""
+          class="h-12 w-12 rounded-full bg-gray-50">
+        <div class="text-sm leading-6">
+          <p class="font-semibold text-gray-900">
+            <a href="#">
+              <span class="absolute inset-0"></span>
+              {{ author[0].fields.name['pt-BR'] }}
+            </a>
+          <p> {{ author[0].fields.image['pt-BR'].fields.file.url }} </p>
+          </p>
+          <div class="blog_post__dates">
+            <p class="blog_post__publication">{{ $t("blog.publication") }}: {{ publicationDate }}</p>
+            <p class="blog_post__revision">{{ $t("blog.revision") }}: {{ revisionDate }}</p>
+          </div>
+        </div>
+      </div>
+
+      <!-- <div class="mt-10 -mx-7 md:mx-0">
+        <img class="w-full max-w-2xl mx-auto" :src="post.img" :width="960" :height="500" :alt="post.altText">
+      </div> -->
+    </header>
 
     <div class="blog_post__body" v-html="blogText" />
 
-    <div class="blog_post__tags">
-      {{ $t("blog.tags") }}:
-      {{ tagsList }}
-    </div>
-  </div>
+  </article>
 </template>
 
 <script setup>
 const { t: $t } = useI18n();
 
-const { publication, revision, title, body, tags } = defineProps([
+const { publication, revision, title, body, tags, author } = defineProps([
   "publication",
   "revision",
   "title",
   "body",
   "tags",
+  "author",
 ]);
 
 const publicationDate = computed(() => new Date(publication).toLocaleDateString());
@@ -43,14 +66,15 @@ const tagsList = computed(() => tags.join(", "));
 }
 
 .blog_post__dates {
-  @apply mt-4;
-  @apply text-lg;
+  @apply text-gray-600;
+  @apply text-xs;
 }
 
 .blog_post__body {
   @apply mt-8;
   @apply text-2xl;
   @apply leading-relaxed;
+  @apply text-slate-800 max-w-none;
 }
 
 .blog_post__body b {
