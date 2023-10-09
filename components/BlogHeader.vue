48dee06155860be5be0664b1754712a151f809a5
sabrizzs <93349105+sabrizzs@users.noreply.github.com>
2023-10-09 15:15:27 -0300

    Update BlogHeader.vue

--- components/BlogHeader.vue
+++ components/BlogHeader.vue
@@ -1,68 +1,32 @@
 <template>
-    <div class="text-gray-600 body-font">
-        <nav class="flex px-4 border-b md:shadow-lg items-center relative">
-            <div class="text-lg font-bold md:py-0 py-4">
-                Auraz Blog
-            </div>
-            <ul class="md:px-2 ml-auto md:flex md:space-x-2 absolute md:relative top-full left-0 right-0">
-                <li>
-                    <a href="#" class="flex md:inline-flex p-4 items-center hover:bg-gray-50">
-                        <span>Home</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="#" class="flex md:inline-flex p-4 items-center hover:bg-gray-50">
-                        <span>Products</span>
-                    </a>
-                </li>
-                <li class="relative parent">
-                    <a href="#" class="flex justify-between md:inline-flex p-4 items-center hover:bg-gray-50 space-x-2">
-                        <span>Service</span>
-                        <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 fill-current pt-1" viewBox="0 0 24 24"><path d="M0 7.33l2.829-2.83 9.175 9.339 9.167-9.339 2.829 2.83-11.996 12.17z"/></svg>
-                    </a>
-                    <ul class="child transition duration-300 md:absolute top-full right-0 md:w-48 bg-white md:rounded-b ">
-                        <li>
-                            <a href="#" class="flex px-4 py-3 hover:bg-gray-50">
-                                Web development
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#" class="flex px-4 py-3 hover:bg-gray-50">
-                                Web Design
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#" class="flex px-4 py-3 hover:bg-gray-50">
-                                Machine Learning
-                            </a>
-                        </li>
-                    </ul>
-                </li>
-                <li>
-                    <a href="#" class="flex md:inline-flex p-4 items-center hover:bg-gray-50">
-                        <span>About us</span>
-                    </a>
+    <div class="bg-white">
+        <div class="text-slate-700 relative flex max-w-screen-xl flex-col overflow-hidden px-4 py-4 md:mx-auto md:flex-row md:items-center">
+            <h3 class="flex cursor-pointer items-center whitespace-nowrap text-2xl font-black">
+            <span class="mr-2 text-4xl text-cyan-500">
+                <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" width="1em" height="1em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><path fill="currentColor" d="M6.925 16.875Q5.2 16.225 4.1 14.713Q3 13.2 3 11.25q0-1.975.938-3.513Q4.875 6.2 6 5.15q1.125-1.05 2.062-1.6L9 3v2.475q0 .625.45 1.062q.45.438 1.075.438q.35 0 .65-.15q.3-.15.5-.425L12 6q.95.55 1.625 1.35t1.025 1.8l-1.675 1.675q-.05-.6-.287-1.175q-.238-.575-.638-1.05q-.35.2-.738.287q-.387.088-.787.088q-1.1 0-1.987-.612Q7.65 7.75 7.25 6.725q-.95.925-1.6 2.062Q5 9.925 5 11.25q0 .775.275 1.462q.275.688.75 1.213q.05-.5.287-.938q.238-.437.588-.787L9 10.1l2.15 2.1q.05.05.1.125t.1.125l-1.425 1.425q-.05-.075-.087-.125q-.038-.05-.088-.1L9 12.925l-.7.7q-.125.125-.212.287q-.088.163-.088.363q0 .3.175.537q.175.238.45.363ZM9 10.1Zm0 0ZM7.4 22L6 20.6L19.6 7L21 8.4L17.4 12H21v2h-5.6l-.5.5l1.5 1.5H21v2h-2.6l2.1 2.1l-1.4 1.4l-2.1-2.1V22h-2v-4.6l-1.5-1.5l-.5.5V22h-2v-3.6Z" /></svg>
+            </span>
+            mellow
+            </h3>
+            <input type="checkbox" class="peer hidden" id="navbar-open" />
+            <label class="absolute top-5 right-7 cursor-pointer md:hidden" for="navbar-open">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16M4 18h16" />
+            </svg>
+            </label>
+            <div class="peer-checked:mt-8 peer-checked:max-h-56 flex max-h-0 w-full flex-col items-center justify-between overflow-hidden transition-all md:ml-24 md:max-h-full md:flex-row md:items-start">
+            <ul class="flex flex-col items-center space-y-2 md:ml-auto md:flex-row md:space-y-0">
+                <li class="font-bold md:mr-12"><a href="#">Blog</a></li>
+                <li class="md:mr-12"><a href="#">Features</a></li>
+                <li class="md:mr-12"><a href="#">Support</a></li>
+                <li class="md:mr-12">
+                <button class="rounded-full border-2 border-cyan-500 px-6 py-1 text-cyan-600 transition-colors hover:bg-cyan-500 hover:text-white">Login</button>
                 </li>
             </ul>
-            
-        </nav>
+            </div>
+        </div>
     </div>
 </template>
 
 <style>
-    @media only screen and (min-width: 768px){
-        .parent:hover .child {
-            opacity:1;
-            height:auto;
-            overflow:none;
-            transform: translateY(0);
-        }
-        .child {
-            opacity:0;
-            height:0;
-            overflow:hidden;
-            transform: translateY(-10%);
-        }
-    }
-    
+
 </style>
