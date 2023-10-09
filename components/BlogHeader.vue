0bcb82ed87a78870c5b7765d6ac00fc52bc4701e
sabrizzs <93349105+sabrizzs@users.noreply.github.com>
2023-10-09 15:58:44 -0300

    Update BlogHeader.vue

--- components/BlogHeader.vue
+++ components/BlogHeader.vue
@@ -12,7 +12,7 @@
             </label>
             <div class="peer-checked:mt-8 peer-checked:max-h-56 flex max-h-0 w-full flex-col items-center justify-between overflow-hidden transition-all md:ml-24 md:max-h-full md:flex-row md:items-start">
             <ul class="flex flex-col items-center space-y-2 md:ml-auto md:flex-row md:space-y-0">
-                <li class="font-bold md:mr-12"><a href="#">Lorem 1</a></li>
+                <li class="body-font md:mr-12"><a href="#">Lorem 1</a></li>
                 <li class="md:mr-12"><a href="#">Lorem 2</a></li>
                 <li class="md:mr-12"><a href="#">Lorem 3</a></li>
                 <li class="md:mr-12">
